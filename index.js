5a136c7cebb652d2e109052dace590d61be56c36
karim <karim@csit.fi>
2020-04-10 01:08:59 +0300

    connect db to mlab

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+
 const express = require('express');
+const connectDB =require('./config/dev');
+
+
 
 const app = express();
 
+// connect Database
+connectDB();
 
 
 const port = process.env.PORT || 3002;
